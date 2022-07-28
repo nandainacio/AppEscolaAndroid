1dafaa8b2de2f3e3aa9c2dad252b1121a2dde291
nandainacio <fp_inacio@hotmail.com>
2022-07-28 07:14:10 -0300

    Set colors according to different grade situations

--- app/src/main/java/com/example/appescola/MainActivity.java
+++ app/src/main/java/com/example/appescola/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.appescola;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -27,19 +28,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void calcular(View view) {
-        float n1= Float.parseFloat(edtN1.getText().toString());
-        float n2 = Float.parseFloat(edtN2.getText().toString());
-        float m = (n1 + n2)/2;
+        boolean ok = true;
 
-        txtM.setText(String.format("%.1f", m));
-
-        if(m < 5){
-            txtSit.setText(getString(R.string.strSitRp));
-        }else if(m < 7){
-            txtSit.setText(getString(R.string.strSitRc));
-        }else{
-            txtSit.setText(getString(R.string.strSitAp));
+        if (edtN1.getText().toString().trim().isEmpty()){
+            ok = false;
+            edtN1.setError(getString(R.string.msgErro));
         }
+        if(edtN2.getText().toString().trim().isEmpty()){
+            ok = false;
+            edtN2.setError(getString(R.string.msgErro));
+        }
+        if(ok) {
+            float n1 = Float.parseFloat(edtN1.getText().toString());
+            float n2 = Float.parseFloat(edtN2.getText().toString());
+            float m = (n1 + n2) / 2;
 
+            txtM.setText(String.format("%.1f", m));
+
+            if (m < 5) {
+                txtSit.setText(getString(R.string.strSitRp));
+                txtSit.setTextColor(getResources().getColor(R.color.reproved_color));
+                txtM.setTextColor(getResources().getColor(R.color.reproved_color));
+            } else if (m < 7) {
+                txtSit.setText(getString(R.string.strSitRc));
+                txtSit.setTextColor(getResources().getColor(R.color.retake_test_color));
+                txtM.setTextColor(getResources().getColor(R.color.retake_test_color));
+            } else {
+                txtSit.setText(getString(R.string.strSitAp));
+                txtSit.setTextColor(getResources().getColor(R.color.approved_color));
+                txtM.setTextColor(getResources().getColor(R.color.approved_color));
+            }
+        }
     }
 }
